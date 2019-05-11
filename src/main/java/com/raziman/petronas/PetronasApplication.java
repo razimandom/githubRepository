be3bb2314f2d6d08bee446ea2fada3b28e1408ad
Raziman Dom <razimandom@gmail.com>
2019-05-11 12:53:02 +0800

    Added spring security for admin login

--- src/main/java/com/raziman/petronas/PetronasApplication.java
+++ src/main/java/com/raziman/petronas/PetronasApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 import com.raziman.petronas.properties.FileStorageProperties;
 
 @SpringBootApplication
+@EnableWebSecurity
 @EnableConfigurationProperties({
 	FileStorageProperties.class
 })
